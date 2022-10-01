1d52402aa6894ec3debd27238a5413b0d118982e
HOVOH <71901758+HOVOH@users.noreply.github.com>
2022-10-01 15:47:00 -0400

    add bond to olympus-pro api

--- packages/olympuspro-api/src/index.ts
+++ packages/olympuspro-api/src/index.ts
@@ -20,6 +20,10 @@ const arbitrumMainnetContracts = {
   WETHGMX: bond('0xdfb034cd2789b9f85e57d25C69E161eA0bdC8f53'),
 };
 
+const mainnetContracts = {
+  WETHIQ: bond('0xd208af5393121B41c5D62e25589561748D87a1D8'),
+};
+
 const namedFactories = {
   CustomBond: bindings(
     CustomBond__factory.connect,
@@ -29,21 +33,23 @@ const namedFactories = {
 
 export interface OlympusProNetworksContractMap {
   [Network.ARBITRUM]: typeof arbitrumMainnetContracts;
+  [Network.MAINNET]: typeof mainnetContracts;
 }
 
-const olympusProApi = new NetworksContractsRegistry<
+export const olympusProApi = new NetworksContractsRegistry<
   OlympusProNetworksContractMap,
   typeof namedFactories
 >();
 
 olympusProApi.addNetwork(Network.ARBITRUM, arbitrumMainnetContracts);
+olympusProApi.addNetwork(Network.MAINNET, mainnetContracts);
 olympusProApi.setNamedFactories(namedFactories);
 
 export type OlympusProAPI = ContractFactory<
   OlympusProNetworksContractMap,
   typeof namedFactories
 >;
-export type ChainlinkNetworks = keyof OlympusProNetworksContractMap;
+export type OlympusProNetworks = keyof OlympusProNetworksContractMap;
 
 export const initOlympusProApi = (
   providers: ProvidersRegistry
